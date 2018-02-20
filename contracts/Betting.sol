e452f094338922d05b136c2b6c5a4b67d73831f0
Ebin Benny <bennye@tcd.ie>
2018-02-20 00:00:08 +0000

    Made the Smart Contract more functional by filling in barebone functions.

--- contracts/Betting.sol
+++ contracts/Betting.sol
@@ -1,43 +1,73 @@
 pragma solidity ^0.4.17;
 
 contract Betting {
+    
+    address public creator;
+    
+    string public outcomeOne;
+    string public outcomeTwo;
+    string public outcomeThree;
+    
+    uint256[] public totalPools;
+    uint256 public winningIndex;
+    bool eventStart;
 
-  address public creator;
-  string outcomeOne;
-  string outcomeTwo;
-  string outcomeThree;
-
-  struct Bet {
-      address addr;
-      uint amount;
-  }
+    struct Bet {
+        uint outcomeIndex;
+        uint256 amount;
+        bool paid;
+    }
+    
+    mapping(address => Bet) public bets;
   
-  uint public testCounter;  
+    modifier onlyCreator() {
+        if (msg.sender == creator) 
+        _ ;
+    }
 
-  modifier onlyCreator() {
-    if (msg.sender == creator) 
-    _ ;
-  }
+    function Betting(string _outcomeOne, string _outcomeTwo, string _outcomeThree) public {
+        creator = msg.sender;
+        outcomeOne = _outcomeOne;
+        outcomeTwo = _outcomeTwo;
+        outcomeThree = _outcomeThree;
+        totalPools = new uint256[](4);
+        winningIndex = 5;
+        eventStart = false;
+    }
+    
+    function placeBet(uint _outcomeIndex) public payable {
+        require(!eventStart);
+        bets[msg.sender] = Bet({outcomeIndex: _outcomeIndex, amount: msg.value, paid: false});
+        totalPools[_outcomeIndex] += msg.value;
+        totalPools[3] += msg.value;
+    }
+    
+    function changeBet(uint _outcomeIndex) public {
+        require(!eventStart);
+        Bet storage previousBet = bets[msg.sender];  
+        totalPools[previousBet.outcomeIndex] -= previousBet.amount;
+        totalPools[_outcomeIndex] += previousBet.amount;
+        previousBet.outcomeIndex = _outcomeIndex;
+        bets[msg.sender] = previousBet;
+    }
 
-  function Betting(string outcomeOne, string outcomeTwo, string outcomeThree) public {
-    creator = msg.sender;
-    testCounter = 0;
-  }
+    function eventStarted() public onlyCreator {
+        eventStart = true;
+    }
 
-  function placeBet(uint outcomeIndex) public payable {
-      testCounter++;
-  }
+    function eventOver(uint _outcomeIndex) public onlyCreator {
+        //todo Use Oraclize to get result from API
+        winningIndex = _outcomeIndex;
+    }
 
-  function changeBet(uint outcomeIndex) public payable {
-      testCounter++;
-  }
-
-  function eventOver() private onlyCreator {
-      testCounter++;
-  }
-
-  function claimWinnings() public {
-      testCounter++;
-  }
+    function claimWinnings() public {
+        Bet storage placedBet = bets[msg.sender];
+        require( placedBet.outcomeIndex == winningIndex);
+        uint256 winnings = placedBet.amount+((((totalPools[3]-totalPools[winningIndex])*(placedBet.amount/totalPools[winningIndex]))*(100-1))/100);
+        require( placedBet.paid == false);
+        placedBet.paid = true;
+        bets[msg.sender] = placedBet;
+        msg.sender.transfer(winnings);
+    }
 
 }
