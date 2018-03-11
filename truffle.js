a3e3574530e6cadc7024d77f9b3e63c910f0f5e8
Ebin Benny <bennye@tcd.ie>
2018-03-11 12:08:07 +0000

    Added Ropsten to networks.

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "127.0.0.1",
       port: "7545",
       network_id: "*"
+    },
+    ropsten: {
+      host: "0.0.0.0",
+      port: "8545",
+      network_id: "3"
     }
   }
 };
